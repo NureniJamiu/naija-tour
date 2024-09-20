067c041f6cc0fa0942cc02a0d19c40b2892ce75b
NureniJamiu <nurenijamiu951@gmail.com>
2024-09-20 06:05:56 +0100

    fix: broken link for images

--- src/db/data.js
+++ src/db/data.js
@@ -416,6 +416,7 @@ export const destinations = [
         "rating": 4.6,
         "price": 5000,
         "images": [
+            "https://propertypro.ng/blog/wp-content/uploads/2018/03/Olumo-rock-780x405.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/7/74/Olumo_Rock.jpg",
             "https://example.com/olumo2.jpg"
         ]
@@ -436,6 +437,7 @@ export const destinations = [
         "rating": 4.3,
         "price": 3000,
         "images": [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Zuma_Rock.jpg/800px-Zuma_Rock.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/8/82/Zuma_Rock.jpg",
             "https://example.com/zuma2.jpg"
         ]
@@ -456,6 +458,7 @@ export const destinations = [
         "rating": 4.6,
         "price": 25000,
         "images": [
+            "https://goodnigeria.com/wp-content/uploads/2020/02/Yankari.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/e/ea/Yankari_National_Park.jpg",
             "https://example.com/yankari2.jpg"
         ]
@@ -476,6 +479,7 @@ export const destinations = [
         "rating": 4.5,
         "price": 40000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmnlcnsS4Hvt8ouivo-C04zFZ4wOndARjjLQ&s",
             "https://upload.wikimedia.org/wikipedia/commons/2/28/Kajuru_Castle.jpg",
             "https://example.com/kajuru2.jpg"
         ]
@@ -496,6 +500,7 @@ export const destinations = [
         "rating": 4.7,
         "price": 8000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXzrAqVdszoudC3LTL63w4_AHzEpXd6Jlfpw&s",
             "https://upload.wikimedia.org/wikipedia/commons/c/c8/Erin_Ijesha_Waterfalls.jpg",
             "https://example.com/erin2.jpg"
         ]
@@ -516,6 +521,7 @@ export const destinations = [
         "rating": 4.2,
         "price": 5000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCCAm6ZrVVugOg0eSQmyvGQialgjv_A-PclQ&s",
             "https://upload.wikimedia.org/wikipedia/commons/6/6b/Oguta_Lake.jpg",
             "https://example.com/oguta2.jpg"
         ]
@@ -536,6 +542,7 @@ export const destinations = [
         "rating": 4.5,
         "price": 3000,
         "images": [
+            "https://upload.wikimedia.org/wikipedia/commons/0/08/Awhum_Water_Fall_Enugu%2C_Nigeria.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/9/9c/Awhum_Waterfall.jpg",
             "https://example.com/awhum2.jpg"
         ]
@@ -556,6 +563,7 @@ export const destinations = [
         "rating": 4.8,
         "price": 15000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1dYsg8n3mQhhsMBRvCsw4J6qHS8nNm0Khgg&s",
             "https://upload.wikimedia.org/wikipedia/commons/7/70/Mambilla_Plateau.jpg",
             "https://example.com/mambilla2.jpg"
         ]
@@ -576,6 +584,7 @@ export const destinations = [
         "rating": 4.3,
         "price": 2000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZeQlmm4T_CNDdDTBKK7y-2pTrl8OymJl-gQ&s",
             "https://upload.wikimedia.org/wikipedia/commons/1/1b/Bar_Beach_Lagos.jpg",
             "https://example.com/barbeach2.jpg"
         ]
@@ -596,6 +605,7 @@ export const destinations = [
         "rating": 4.7,
         "price": 4000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF3dZfbfPj1lXzZKPTHrpq3PNugcQkjRWCmw&s",
             "https://upload.wikimedia.org/wikipedia/commons/3/3c/Lekki_Conservation_Centre.jpg",
             "https://example.com/lekki2.jpg"
         ]
@@ -616,6 +626,7 @@ export const destinations = [
         "rating": 4.4,
         "price": 2500,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBG7IfNQQGfJR9q4LEvLCHqSz16ADHY93YQg&s",
             "https://upload.wikimedia.org/wikipedia/commons/2/2f/Ogbunike_Caves.jpg",
             "https://example.com/ogbunike2.jpg"
         ]
@@ -636,6 +647,7 @@ export const destinations = [
         "rating": 4.5,
         "price": 0,
         "images": [
+            "https://www.alluringworld.com/wp-content/uploads/2018/02/5-Aso-Rock-700x440.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/5/5d/Aso_Rock.jpg",
             "https://example.com/asorock2.jpg"
         ]
@@ -656,6 +668,7 @@ export const destinations = [
         "rating": 4.3,
         "price": 20000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjHyuQ4I5R9VE50bvfKEEd4_4NArrMBBVTTw&s",
             "https://upload.wikimedia.org/wikipedia/commons/d/d6/Kainji_Lake_National_Park.jpg",
             "https://example.com/kainji2.jpg"
         ]
@@ -676,6 +689,7 @@ export const destinations = [
         "rating": 4.6,
         "price": 4000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwbohQMzciMQD-M_hOrWDw4oV8SeIwfJFbeQ&s",
             "https://upload.wikimedia.org/wikipedia/commons/3/37/Idanre_Hills.jpg",
             "https://example.com/idanre2.jpg"
         ]
@@ -696,6 +710,7 @@ export const destinations = [
         "rating": 4.8,
         "price": 6000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB9S399SU3RalwhdqTX7ZBZspwiXtubP2TcA&s",
             "https://upload.wikimedia.org/wikipedia/commons/2/24/Sukur_Cultural_Landscape.jpg",
             "https://example.com/sukur2.jpg"
         ]
@@ -716,6 +731,7 @@ export const destinations = [
         "rating": 4.4,
         "price": 25000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3lij_uVqhr_jhZTrDifiM1OUDFBtT3_6RYg&s",
             "https://upload.wikimedia.org/wikipedia/commons/8/8d/Tinapa_Resort.jpg",
             "https://example.com/tinapa2.jpg"
         ]
@@ -736,6 +752,7 @@ export const destinations = [
         "rating": 4.2,
         "price": 1500,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrhZEjq-NwDY55nONxHkwFpxcLefQoOUq5eA&s",
             "https://upload.wikimedia.org/wikipedia/commons/a/ab/National_Museum_Lagos.jpg",
             "https://example.com/nationalmuseum2.jpg"
         ]
@@ -756,6 +773,7 @@ export const destinations = [
         "rating": 4.7,
         "price": 15000,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxTdGqAyCvEbfZ984SYgw-E37_uTSVewRQOA&s",
             "https://upload.wikimedia.org/wikipedia/commons/1/1c/Gashaka_Gumti_National_Park.jpg",
             "https://example.com/gashaka2.jpg"
         ]
@@ -776,50 +794,52 @@ export const destinations = [
         "rating": 4.5,
         "price": 3000,
         "images": [
+            "https://propertyfinder.com.ng/wp-content/uploads/2023/09/lekki-conservation-centre.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/4/45/Lekki_Conservation_Centre.jpg",
             "https://example.com/lekki2.jpg"
         ]
     },
-    {
-        "id": 40,
-        "name": "Erin Ijesha Waterfalls",
-        "city": "Erin Ijesha",
-        "state": "Osun",
-        "region": "South-West",
-        "type": "Natural Wonder",
-        "attractionTypes": ["Nature", "Adventure"],
-        "description": "A stunning waterfall located in Erin Ijesha, known for its scenic beauty.",
-        "activities": [
-            "Nature",
-            "Adventure"
-        ],
-        "rating": 4.6,
-        "price": 2000,
-        "images": [
-            "https://upload.wikimedia.org/wikipedia/commons/f/fb/Erin_Ijesha_Waterfalls.jpg",
-            "https://example.com/erinjiesha2.jpg"
-        ]
-    },
-    {
-        "id": 41,
-        "name": "Obudu Mountain Resort",
-        "city": "Obudu",
-        "state": "Cross River",
-        "region": "South-South",
-        "type": "Resort",
-        "attractionTypes": ["Leisure", "Adventure"],
-        "description": "A mountain resort offering a range of leisure and adventure activities with stunning views.",
-        "activities": [
-            "Leisure",
-            "Adventure"
-        ],
-        "rating": 4.7,
-        "price": 35000,
-        "images": [
-            "https://upload.wikimedia.org/wikipedia/commons/1/12/Obudu_Mountain_Resort.jpg",
-            "https://example.com/obudu2.jpg"
-        ]
-    },
+    // {
+    //     "id": 40,
+    //     "name": "Erin Ijesha Waterfalls",
+    //     "city": "Erin Ijesha",
+    //     "state": "Osun",
+    //     "region": "South-West",
+    //     "type": "Natural Wonder",
+    //     "attractionTypes": ["Nature", "Adventure"],
+    //     "description": "A stunning waterfall located in Erin Ijesha, known for its scenic beauty.",
+    //     "activities": [
+    //         "Nature",
+    //         "Adventure"
+    //     ],
+    //     "rating": 4.6,
+    //     "price": 2000,
+    //     "images": [
+    //         "https://upload.wikimedia.org/wikipedia/commons/f/fb/Erin_Ijesha_Waterfalls.jpg",
+    //         "https://example.com/erinjiesha2.jpg"
+    //     ]
+    //  },
+
+    // {
+    //     "id": 41,
+    //     "name": "Obudu Mountain Resort",
+    //     "city": "Obudu",
+    //     "state": "Cross River",
+    //     "region": "South-South",
+    //     "type": "Resort",
+    //     "attractionTypes": ["Leisure", "Adventure"],
+    //     "description": "A mountain resort offering a range of leisure and adventure activities with stunning views.",
+    //     "activities": [
+    //         "Leisure",
+    //         "Adventure"
+    //     ],
+    //     "rating": 4.7,
+    //     "price": 35000,
+    //     "images": [
+    //         "https://upload.wikimedia.org/wikipedia/commons/1/12/Obudu_Mountain_Resort.jpg",
+    //         "https://example.com/obudu2.jpg"
+    //     ]
+    // //},
     {
         "id": 42,
         "name": "Ogbomosho",
@@ -836,30 +856,33 @@ export const destinations = [
         "rating": 4.3,
         "price": 0,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg0IbhRe4tYBiVpKIT2IZazuoH5qImZ8D0WA&s",
             "https://upload.wikimedia.org/wikipedia/commons/5/5f/Ogbomosho.jpg",
             "https://example.com/ogbomosho2.jpg"
         ]
     },
-    {
-        "id": 43,
-        "name": "Mambilla Plateau",
-        "city": "Mambilla",
-        "state": "Taraba",
-        "region": "North-East",
-        "type": "Natural Wonder",
-        "attractionTypes": ["Nature", "Adventure"],
-        "description": "A highland area with breathtaking landscapes and opportunities for adventure.",
-        "activities": [
-            "Nature",
-            "Adventure"
-        ],
-        "rating": 4.8,
-        "price": 10000,
-        "images": [
-            "https://upload.wikimedia.org/wikipedia/commons/6/63/Mambilla_Plateau.jpg",
-            "https://example.com/mambilla2.jpg"
-        ]
-    },
+
+    // {
+    //     "id": 43,
+    //     "name": "Mambilla Plateau",
+    //     "city": "Mambilla",
+    //     "state": "Taraba",
+    //     "region": "North-East",
+    //     "type": "Natural Wonder",
+    //     "attractionTypes": ["Nature", "Adventure"],
+    //     "description": "A highland area with breathtaking landscapes and opportunities for adventure.",
+    //     "activities": [
+    //         "Nature",
+    //         "Adventure"
+    //     ],
+    //     "rating": 4.8,
+    //     "price": 10000,
+    //     "images": [
+    //         "https://upload.wikimedia.org/wikipedia/commons/6/63/Mambilla_Plateau.jpg",
+    //         "https://example.com/mambilla2.jpg"
+    //     ]
+    // },
+
     {
         "id": 44,
         "name": "Nigerian National Mosque",
@@ -876,6 +899,7 @@ export const destinations = [
         "rating": 4.6,
         "price": 0,
         "images": [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqjAxSxOKUJ_ZLawRBvlLBHwSN7hjoE_Igkg&s",
             "https://upload.wikimedia.org/wikipedia/commons/4/4d/Nigerian_National_Mosque.jpg",
             "https://example.com/nationalmosque2.jpg"
         ]
@@ -896,6 +920,7 @@ export const destinations = [
         "rating": 4.2,
         "price": 1000,
         "images": [
+            "https://21stcenturychronicle.com/wp-content/uploads/2021/08/More-works-on-display.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/a/a6/Bida_Pottery.jpg",
             "https://example.com/bida2.jpg"
         ]
